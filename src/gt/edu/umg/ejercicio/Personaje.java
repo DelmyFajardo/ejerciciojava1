8e39c40312aa577ad46058086f3404a517fae94d
Delmy <dfajardob@miumg.edu.gt>
2024-07-22 22:04:52 -0600

    personaje java

---
+++ src/gt/edu/umg/ejercicio/Personaje.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ejercicio;
+
+public class Personaje {
+    private String nombre;
+    private String descripcion;
+    private long tamano;
+    private int poder;
+    private int vida;
+
+    public void desplazarse (){}
+    public int mostrarVida;
+    public void mostrarPoder(){}
+}
